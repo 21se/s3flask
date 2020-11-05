545dd299449cdcbef7bde1ee616b56d0426a0d87
Артём Аборнев <21se90210@gmail.com>
2020-11-05 22:17:30 +0700

    refactor

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, Response, request, render_template
 import requests
 import csv
 import os
+import xmltodict
 
 app = Flask(__name__)
 
-def readGuids(filename):
+
+def read_guids(filename):
     dictionary = {}
 
     try:
@@ -22,16 +24,17 @@ def readGuids(filename):
     return dictionary
 
 
-def writeGuids(filename):
+def write_guids(filename):
     with open(os.getcwd() + '\\' + filename, mode='w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
-        writer.writerows(objectsGuids.items())
+        writer.writerows(objects_guids.items())
 
 
-objectsGuids = readGuids('objectsGUID.csv')
+objects_guids = read_guids('objectsGUID.csv')
+
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH', 'OPTIONS'])
-def routehome():
+def route_home():
     return proxy()
 
 
@@ -41,11 +44,11 @@ def route(path):
 
 
 # @app.route('/copy', methods=['GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH', 'OPTIONS'])
-# def routecopy():
+# def route_copy():
 
 
-@app.route('/findksu', methods=['GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH', 'OPTIONS'])
-def routefind():
+@app.route('/find_ksu', methods=['GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH', 'OPTIONS'])
+def route_find():
     return render_template('find.html')
 
 
@@ -63,12 +66,20 @@ def proxy():
     out = Response(generate(), headers=headers)
     out.status_code = r.status_code
 
+    if out.data:
+        datadict = xmltodict.parse(out.data)
+        list_bucket_result = datadict.get('ListBucketResult')
+        if list_bucket_result:
+            contents = list_bucket_result.get('Contents', {})
+            for content in contents:
+                a = content
+
     if out.status_code == 200:
         if request.method == 'PUT':
             guid = request.headers.environ.get('HTTP_X_AMZ_META_GUID1C')
             if guid:
-                objectsGuids[request.path] = guid
-                writeGuids('objectsGUID.csv')
+                objects_guids[request.path] = guid
+                write_guids('objectsGUID.csv')
 
     return out
 
