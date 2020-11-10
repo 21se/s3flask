381061359875469e711c811c2eee6366f318e779
Артём Аборнев <21se90210@gmail.com>
2020-11-10 21:24:04 +0700

    redis

--- app.py
+++ app.py
@@ -1,36 +1,11 @@
+import redis as redis_client
 from flask import Flask, Response, request, render_template
 import requests
-import csv
-import os
 import xmltodict
 
+
 app = Flask(__name__)
-
-
-def read_guids(filename):
-    dictionary = {}
-
-    try:
-        with open(os.getcwd() + '\\' + filename, mode='r', newline='') as file:
-            reader = csv.reader(file, delimiter=';')
-            for rows in reader:
-                key = rows[0]
-                value = rows[1]
-
-                dictionary[key] = value
-    except:
-        return dictionary
-
-    return dictionary
-
-
-def write_guids(filename):
-    with open(os.getcwd() + '\\' + filename, mode='w', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerows(objects_guids.items())
-
-
-objects_guids = read_guids('objectsGUID.csv')
+redis = redis_client.Redis('localhost', 6379, 0)
 
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH', 'OPTIONS'])
@@ -67,19 +42,26 @@ def proxy():
     out.status_code = r.status_code
 
     if out.data:
-        datadict = xmltodict.parse(out.data)
-        list_bucket_result = datadict.get('ListBucketResult')
-        if list_bucket_result:
-            contents = list_bucket_result.get('Contents', {})
-            for content in contents:
-                a = content
+        try:
+            new_items = []
+            datadict = xmltodict.parse(out.data)
+            list_bucket_result = datadict.get('ListBucketResult')
+            if list_bucket_result:
+                contents = list_bucket_result.get('Contents', {})
+                for content in contents:
+                    new_items.append(content)
+                for item in new_items:
+                    contents.append(item)
+                out.data = xmltodict.unparse(datadict)
+        except Exception as error:
+            print(error)
 
     if out.status_code == 200:
         if request.method == 'PUT':
             guid = request.headers.environ.get('HTTP_X_AMZ_META_GUID1C')
             if guid:
-                objects_guids[request.path] = guid
-                write_guids('objectsGUID.csv')
+                if redis.set(request.path + ':guid1c:' + guid, '1'):
+                    redis.save()
 
     return out
 
